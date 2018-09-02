7b53a9b5c205a5cbe6e072d139fcf272687a7ec0
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2018-09-02 18:29:49 -0400

    enforcing D.R.Y. :  clever downsizing

--- qc_glassdoor_parser.py
+++ qc_glassdoor_parser.py
@@ -16,34 +16,29 @@ class GlassdoorQcParser:
         self.__save_offers_glassdoor(jobs, urls)
 
     def __retrieve_glassdoor_jobs(self):
-        soup = BeautifulSoup(self.__response().content, 'lxml')
-        job_title = soup.findAll("a", {"class": "jobLink"})
-
         titles = []
-        for title in job_title:
-            titles.append(title.text)
+        for item in self.__retrieve_soup():
+            titles.append(item.text)
 
         return list(titles)
 
     def __retrieve_glassdoor_jobs_url(self):
-        soup = BeautifulSoup(self.__response().content, 'lxml')
-        job_ads = soup.findAll("a", {"class": "jobLink"})
-
         url_list = []
-        for url_link in job_ads:
-            url_list.append(self.GLASSDOOR_BASE_URL + url_link['href'])
+        for item in self.__retrieve_soup():
+            url_list.append(self.GLASSDOOR_BASE_URL + item['href'])
 
         return list(url_list)
 
-    def __save_offers_glassdoor(self, jobs, urls):
-        temp_dict_job_url = dict(zip(jobs, urls))
+    def __retrieve_soup(self):
+        soup = BeautifulSoup(self.__response().content, 'lxml')
+        return soup.findAll("a", {"class": "jobLink"})
 
-        for key, value in temp_dict_job_url.items():
+    def __save_offers_glassdoor(self, jobs, urls):
+        for key, value in dict(zip(jobs, urls)).items():
             self.__database.save(key, 'unavailable due to hard to parse HTML ;)', value)
 
     def __response(self):
         # website refuses connection to a crawler agent (HTTP error 403)
         # assign a phony agent to the Request
         #
-        agent = 'Mozilla/5.0'
-        return requests.get(self.GLASSDOOR_QC,  headers={'User-Agent': agent})
+        return requests.get(self.GLASSDOOR_QC,  headers={'User-Agent': 'Mozilla/5.0'})
