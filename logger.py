bda5fdd7f06492f9d13dc8c71604f6d4c870afca
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2019-05-18 17:12:33 -0400

    will need logging !

---
+++ logger.py
@@ -0,0 +1,40 @@
+import logging
+import datetime
+
+
+class Logger:
+
+    LOG_FILE = 'log.out'
+    
+    # logging verbosity can be set from a console command parameter 
+    #   ex. : DEBUG verbosity -> $python mainFile.py 10
+    #   
+    # invokation with parameter = default level of verbosity is ERROR
+    #
+    # in mainFile, you can parse args this way :
+    #
+    #       if len(sys.argv) < 2:
+    #           obj = Logger(sys.argv[0])
+    #       else:
+    #           obj = Logger(sys.argv[0], int(sys.argv[1]))
+
+    def __init__(self, program_name, verbosity=logging.ERROR):
+        # ERROR level by default = integer of value 40
+        # DEBUG level = integer of value 10 inputed by Class user 
+        #
+        logging.basicConfig(filename=self.LOG_FILE, 
+                            level=verbosity
+                            )
+
+        logging.info('Log for program %s' % program_name)
+
+        now =  str(datetime.datetime.today())
+        logging.info('Execution date = %s' % now)
+
+    def debug(self, arg):
+        logging.debug(arg)
+
+    def error(self, arg):
+        now =  str(datetime.datetime.today())
+        logging.error(now + ' ' + arg)
+    
